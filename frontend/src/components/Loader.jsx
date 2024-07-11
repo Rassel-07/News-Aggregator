9ffde28f9e00545b74e2a5fd45ad04ffaad29931
Rassel-07 <razelsadat@gmail.com>
2024-07-12 03:34:38 +0530

    changes in frontend

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import LoadingGif from "../images/loading.gif"
+import LoadingGif from "../images/loading copy.gif"
 export default function Loader(){
     return(
         
         <div className="loader">
             <div className="loader__image">
                 <img src={LoadingGif} alt="" />
-                
             </div>
         </div>
     )
