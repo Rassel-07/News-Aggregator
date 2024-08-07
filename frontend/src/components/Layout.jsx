cd1fd620af6a95bf455c265a61d187daf95e4f05
Rassel-07 <razelsadat@gmail.com>
2024-08-07 23:11:32 +0530

    frontend changes

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -2,15 +2,17 @@ import React from "react"
 import {Outlet} from "react-router-dom"
 import Header from "./Header"
 import Footer from "./Footer"
+import {useContext} from "react"
+import { UserContext } from "../context/userContext";
 
 export default function Layout(){
+    const{currentUser} = useContext(UserContext)
     return(
         <div>
 
             <Header/>
                 <Outlet/>
-            <Footer/>
-
+          {currentUser?.id && <Footer/>}
         </div>
     )
 }
